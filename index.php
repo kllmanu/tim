a69009ad377cddb4c1c6e15866bb10823e619a67
Manuel Koell <manuel.koell@towa.at>
2022-05-28 23:01:58 +0200

    remove twig function (not working as expected)

--- index.php
+++ index.php
@@ -8,16 +8,6 @@
  * Author URI:      https://towagmbh.slack.com/team/U038FQ8LMUJ
  */
 
-function tt($var){
-
-}
-
-add_filter('timber/twig', function($twig) {
-	$twig->addFunction(new Timber\TwigFunction('tt', 'tt'));
-	
-	return $twig;
-});
-
 add_filter('timber_render_data', function ($data) {
 	file_put_contents(plugin_dir_path(__FILE__) . 'context.json', json_encode($data));
 
