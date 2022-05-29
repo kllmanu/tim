34995b1ea0709282f171f4a3188e1e108d119428
Manuel Koell <manuel.koell@towa.at>
2022-05-29 10:21:21 +0200

    beautify json encode

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ add_filter('timber_render_data', function($data) use($file) {
 	$current_slug = add_query_arg(array(), $wp->request);
 
 	if(strpos($current_slug, "manifest") === false) {
-		file_put_contents($file, json_encode($data));
+		file_put_contents($file, json_encode($data, JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES|JSON_NUMERIC_CHECK));
 	}
 
 	return $data;
