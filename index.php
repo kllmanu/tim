ff67e17d87fd527d513f741cc5e298d51ba9f995
Manuel Koell <manuel.koell@towa.at>
2022-05-29 10:03:16 +0200

    fix #3 and write context file only once

--- index.php
+++ index.php
@@ -4,12 +4,19 @@
  * Plugin Name:     Tim
  * Plugin URI:      https://github.com/kllmanu/tim
  * Description:     A timber context command line debugger
- * Author:          manu
- * Author URI:      https://towagmbh.slack.com/team/U038FQ8LMUJ
  */
 
-add_filter('timber_render_data', function ($data) {
-	file_put_contents(plugin_dir_path(__FILE__) . 'context.json', json_encode($data));
+$dir = plugin_dir_path(__FILE__);
+$file = $dir . 'context.json';
+
+add_filter('timber_render_data', function($data) use($file) {
+
+	global $wp;
+	$current_slug = add_query_arg(array(), $wp->request);
+
+	if(strpos($current_slug, "manifest") === false) {
+		file_put_contents($file, json_encode($data));
+	}
 
 	return $data;
 });
